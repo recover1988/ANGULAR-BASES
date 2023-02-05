1465a587111b3762ed9898b8172a3d478eb75756
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-02-05 16:43:23 -0300

    app.component.gtml and app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+//Decorador componente
+// este espera un argumento q es un objeto
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: 'app-root', // nombre que va a tener el componente
+  templateUrl: './app.component.html', // el templateURL es la direccion dela parte HTML, con el path
+  // template:'<span>Eric</span>', // en el template se puede escribir el codigo HTML directamente, antes se aconsejaba usar si el HTML tenia menos de 3 lineas
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bases';
+  title = 'Contador App';
 }
