08109453120208003068311b8cbc5a252d78de26
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-02-05 18:59:36 -0300

    creando folder contador

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ContadorComponent } from './contador.component';
+import { ContadorComponent } from './contador/contador.component';
 // en declarations se colocan los modulos que son las clases que se quieren exportar y usar
 @NgModule({
   // componentes
