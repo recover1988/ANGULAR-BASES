1804ac7507109cbfd49dd1e591050e35b0e9717d
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-02-06 21:25:02 -0300

    one way data binding

--- src/app/heroes/heroe/heroe.component.ts
+++ src/app/heroes/heroe/heroe.component.ts
@@ -8,11 +8,20 @@ export class HeroeComponent {
     nombre: string = 'Ironman';
     edad: number = 45;
 
-    get nombreCapitalizado(){
+    get nombreCapitalizado(): string {
         return this.nombre.toUpperCase();
-    }
+    };
 
-    obtenerNombre() {
+    obtenerNombre(): string {
         return `${this.nombre} - ${this.edad}`
     };
+
+    cambiarNombre(): void {
+        this.nombre = 'Spiderman';
+    };
+
+    cambiarEdad(): void {
+        this.edad = 30;
+    };
 }
+// One way data binding o enlzado en una sola via --> quiere decir que angular solo refresca si algo cambia
