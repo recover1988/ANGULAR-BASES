4189adc0d3778c3ca039ed99cb417bb9bd29ca82
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-02-07 21:27:55 -0300

    2-way-data [(ngModel)]

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+interface Personaje {
+  nombre: string;
+  poder: number;
+}
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
 })
 export class MainPageComponent {
-  agregar() {
-    console.log('Esta es una prueba')
+  nuevo: Personaje = {
+    nombre: 'Trunks',
+    poder: 900
   }
+  agregar() {
+    console.log(this.nuevo)
+  }
+  // cambiarNombre(event: any) {
+  //   console.log(event.target.value)
+  // }
 }
