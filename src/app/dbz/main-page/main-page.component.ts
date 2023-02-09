e47789b49ee5ccbe516287471f26f061027ed0b5
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-02-09 00:42:10 -0300

    delete exports

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 
 import { Personaje } from '../interfaces/dbz.interface';
-import { DbzService } from '../services/dbz.service';
 
 @Component({
   selector: 'app-main-page',
