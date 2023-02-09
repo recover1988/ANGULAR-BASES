f7020e94ffff657bf8e00f3e1b79f9b512e6d951
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-02-08 22:06:18 -0300

    change component agregar

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -22,21 +22,4 @@ export class MainPageComponent {
     },
   ];
 
-  nuevo: Personaje = {
-    nombre: '',
-    poder: 0
-  }
-
-  // agregar() {
-  //   if (this.nuevo.nombre.trim().length === 0) return;
-  //   console.log(this.nuevo)
-  //   this.personajes.push(this.nuevo)
-  //   this.nuevo = {
-  //     nombre: '',
-  //     poder: 0
-  //   }
-  // }
-  // cambiarNombre(event: any) {
-  //   console.log(event.target.value)
-  // }
 }
